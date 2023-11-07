e87f9a2ee4020b3cb4bc641dc9ef936d1f067ee2
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-11-08 00:15:23 +0900

    ✨ microCMSの対応

---
+++ src/libs/microcms.ts
@@ -0,0 +1,12 @@
+import { createClient } from "microcms-js-sdk";
+import type { MicroCMSQueries } from "microcms-js-sdk";
+
+
+const client = createClient({
+    serviceDomain: import.meta.env.SERVICE_DOMAIN,
+    apiKey: import.meta.env.API_KEY,
+});
+
+export const getBlogs = async (queries: MicroCMSQueries) => {
+    return await client.get({ endpoint: "blogs", queries });
+};
